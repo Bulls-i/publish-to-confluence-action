06728719bc3cc072d32a4253168a232c82c5815f
Safi-Bouziane <safi.bouziane@outlook.be>
2023-10-11 10:19:36 +0200

    automated creation of parent page

--- __main__.py
+++ __main__.py
@@ -7,11 +7,12 @@ url = os.getenv("CONFLUENCE_URL")
 token = os.getenv("API_TOKEN")
 parent_id = os.getenv("PARENT_ID")
 directory = os.getenv("FOLDER")
+title = os.getenv("PROJECT_TITLE")
 
 
 def main():
     confluence = utils.authenticate(url, user, token)
-    utils.convert_md_to_html(directory, confluence, parent_id)
+    utils.convert_md_to_html(directory, confluence, parent_id, title)
 
 if __name__ == '__main__':
     main()
--- confluence_api_utils/utils.py
+++ confluence_api_utils/utils.py
@@ -27,15 +27,28 @@ def try_creating_page(confluence_auth: Confluence, parent_id, space_id, title, b
             "incoming request is incomplete or otherwise invalid. For example an incorrect parent id."
         )
 
+def create_parent_page(confluence_auth: Confluence, parent_id, space_id, title):
+    try:
+        if confluence_auth.page_exists(space_id, title):
+            print("Parent page already exists for this project")
+        else:
+            confluence_auth.create_page(space_id, title, "{}", parent_id)
+            print("Created parent page successfully")
+    except HTTPError:
+        print(
+            "incoming request is incomplete or otherwise invalid. For example an incorrect parent id."
+        )
+    page_id = confluence_auth.get_page_id(space_id, title)
+    return page_id
 
-def convert_md_to_html(directory, confluence_auth: Confluence, parent_id):
-    project_title = confluence_auth.get_page_by_id(parent_id)
+def convert_md_to_html(directory, confluence_auth: Confluence, parent_id, title):
     space_id = confluence_auth.get_page_space(parent_id)
+    parent_id = create_parent_page(confluence_auth, parent_id,space_id,title, '')
     pattern = "*.md"
 
     for path, subdirs, files in os.walk(directory):
         if path is not directory:
-            title = f"{project_title['title']}_{path.partition(directory)[-1]}"
+            title = f"{title}_{path.partition(directory)[-1]}"
             try_creating_page(confluence_auth, parent_id, space_id, title, "")
             new_parent_id = confluence_auth.get_page_id(space_id, title)
             parent_id = new_parent_id
@@ -52,6 +65,6 @@ def convert_md_to_html(directory, confluence_auth: Confluence, parent_id):
                         confluence_auth,
                         parent_id,
                         space_id,
-                        f"{project_title['title']}_{name[:-3]}",
+                        f"{title}_{name[:-3]}",
                         html,
                     )
